fc9de9a9ec930055c673d4b7681003bfb54145ae
Ana Lisboa <ana.lisboa@clickbus.com>
2021-10-07 09:38:45 -0300

    finished last basic layout changes

--- app/src/main/java/com/megganbz/movieappretrofitapiconnection/MovieFragment.kt
+++ app/src/main/java/com/megganbz/movieappretrofitapiconnection/MovieFragment.kt
@@ -32,7 +32,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
     companion object {
