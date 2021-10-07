2565b8ddedb6ccc1ffcabe5247cd92f086bb7c7f
Ana Lisboa <ana.lisboa@clickbus.com>
2021-10-07 16:40:03 -0300

    Added viewbinding to the project and setup bottomnavigation

--- app/src/main/java/com/megganbz/movieappretrofitapiconnection/MainActivity.kt
+++ app/src/main/java/com/megganbz/movieappretrofitapiconnection/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.megganbz.movieappretrofitapiconnection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.megganbz.movieappretrofitapiconnection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupViewBinding()
 
+        setupNavController()
+        setupBottomNavigation()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.findNavController()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun setupViewBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
